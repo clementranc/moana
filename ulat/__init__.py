72fa7551a543cd168461f6400184b5ac8ea50fe6
Clément Ranc <clement.ranc@gmail.com>
2020-02-19 15:44:27 -0500

    Add import of the Microlens class.

--- ulat/__init__.py
+++ ulat/__init__.py
@@ -1,7 +1,8 @@
 from ._version import version_info, __version__
 
 from ulat.frames import LensReferenceFrame
+from ulat.lens import Microlens
 from ulat.dbc import tools, io, instruments
 
 
-__all__ = ['LensReferenceFrame']
+__all__ = ['LensReferenceFrame', 'Microlens']
