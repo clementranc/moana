57855c5f50ae218d4bdc98da2f109adc3bb19072
Clément Ranc <clement.ranc@gmail.com>
2020-11-07 13:56:18 +0100

    Add SampledPosterior

--- moana/__init__.py
+++ moana/__init__.py
@@ -1,8 +1,10 @@
 from ._version import version_info, __version__
 
+from moana.estimators import SampledPosterior
 from moana.frames import LensReferenceFrame
 from moana.lens import Microlens
 from moana.dbc import tools, io, instruments
+from moana.corner import tools
 
 
-__all__ = ['LensReferenceFrame', 'Microlens']
+__all__ = ['LensReferenceFrame', 'Microlens', 'SampledPosterior']
