f554c670be4ba67a07f417bba24047f4673cf36a
Clément Ranc <clement.ranc@gmail.com>
2020-11-07 13:55:09 +0100

    Add conversion mass fraction to mass ratio.

--- moana/dbc/tools.py
+++ moana/dbc/tools.py
@@ -16,3 +16,16 @@ def from_parfile(filename: str, path: str = './') -> dbc.Dataset:
     dataset = filename.split('par')[1]
     dataset = dbc.Dataset(dataset, path=path)
     return dataset
+
+def mass_fration_to_mass_ratio(x: float) -> float :
+    """Compute the planet mass ratio from planet mass ratio.
+
+    Args:
+        x: mass fraction. For a two-body problem with masses M1 and M2, the mass
+            fraction is x = M2 / (M1 + M2), with M2 < M1.
+
+    Return:
+        The corresponding mass ratio, i.e., M2/M1, with M2 < M1.
+
+    """
+    return x / (1 - x)
