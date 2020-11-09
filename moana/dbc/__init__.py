ce36f88257fd800470a4ea21d54b18782db3b890
Clément Ranc <clement.ranc@gmail.com>
2020-11-09 22:45:21 +0100

    Add function custom_floor

--- moana/dbc/__init__.py
+++ moana/dbc/__init__.py
@@ -1,5 +1,5 @@
 from .instruments import Dataset
 from .io import Output
-from .tools import from_parfile, mass_fration_to_mass_ratio
+from .tools import from_parfile, mass_fration_to_mass_ratio, custom_floor
 
 __all__ = ["Dataset", "Output"]
