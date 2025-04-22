fa78dcb31da71a5305da56282c525e91cc0114d6
clement <ranc@iap.fr>
2025-04-22 20:22:28 +0200

    Modernise the doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
