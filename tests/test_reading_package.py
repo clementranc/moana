918f31d1556b8e7e83c5686246fc1fd90f7e1934
stelais <stela.ishitani@gmail.com>
2025-05-20 17:48:24 -0400

    Making moana pip installable as "moana-pypi" (other names can be used). Adding a building test

--- tests/test_reading_package.py
+++ tests/test_reading_package.py
@@ -2,4 +2,4 @@ from moana.lens import ResonantCaustic
 
 def test_causitc_creation():
     caustic = ResonantCaustic(sep= 1.0, q= 1e-3)
-    assert caustic.xcoords.shape[0] > 10
+    assert caustic.xcoords.shape[0] > 1
